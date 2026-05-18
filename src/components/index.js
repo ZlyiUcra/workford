2055b4165fa960bf760fa7435edf03de8a6a90fc
AlexGochu <gochualex@gmail.com>
2026-05-19 00:55:44 +0100

    Implements base page with all structures, contents and styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 export { default as AllGallery } from "./AllGallery";
 export { default as Banners } from "./Banners";
+export { default as Carousel } from "./Carousel";
 export { default as InstagramStories } from "./InstagramStories";
 export { default as ModalWindow } from "./ModalWindow";
+export { default as Review } from "./Review";
 export { default as YouTubeDesign } from "./YouTubeDesign";
 export { default as YouTubeThumbnails } from "./YouTubeThumbnails";
-export { default as Carousel } from "./Carousel";
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+
 import {
   ModalWindow,
   AllGallery,
   Banners,
   InstagramStories,
+  Review,
   YouTubeThumbnails,
   YouTubeDesign,
 } from "./components";
@@ -25,6 +27,27 @@ const CATEGORY_LABELS: Record<Category, string> = {
   InstagramStories: "Instagram stories",
 };
 
+const reviews = [
+  <Review
+    key={1}
+    name="Vladislav M."
+    link="https://t.me"
+    text="A review left by a client in a Telegram channel, which can be opened by clicking the button in the top right corner of this block."
+  />,
+  <Review
+    key={2}
+    name="Maxim L."
+    link="https://t.me"
+    text="A review left by Maxim"
+  />,
+  <Review
+    key={3}
+    name="Vyacheslav K."
+    link="https://t.me"
+    text="Subscribe to the Telegram channel t.me"
+  />,
+];
+
 function Main() {
   const [showModal, setShowModal] = useState(false);
   const [selectedCategory, setSelectedCategory] =
@@ -38,6 +61,55 @@ function Main() {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+
+  const containerReviewRef = useRef<HTMLDivElement>(null);
+  const reviewWidthRef = useRef(0);
+  const visibleReviews = 3;
+  const handleScroll = () => {
+    const box = containerReviewRef.current;
+    if (!box) return;
+    const loopWidth = reviewWidthRef.current * visibleReviews;
+
+    if (box.scrollLeft <= 0) {
+      box.style.scrollBehavior = "auto";
+      box.scrollLeft += loopWidth;
+      box.style.scrollBehavior = "smooth";
+    }
+
+    if (box.scrollLeft >= loopWidth * 2) {
+      box.style.scrollBehavior = "auto";
+      box.scrollLeft -= loopWidth;
+      box.style.scrollBehavior = "smooth";
+    }
+  };
+
+  const btnPrevReview = () => {
+    const box = containerReviewRef.current;
+    if (!box) return;
+    box.scrollLeft -= reviewWidthRef.current;
+  };
+
+  const btnNextReview = () => {
+    const box = containerReviewRef.current;
+    if (!box) return;
+    box.scrollLeft += reviewWidthRef.current;
+  };
+
+  useEffect(() => {
+    const box = containerReviewRef.current;
+    if (!box) return;
+    const firstReview = box.querySelector(".review-card");
+    if (!firstReview) return;
+    reviewWidthRef.current = firstReview.clientWidth;
+    const width = reviewWidthRef.current * visibleReviews;
+
+    box.scrollLeft = (box.scrollWidth - width) / 2;
+    box.addEventListener("scroll", handleScroll);
+
+    return () => {
+      box.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div>
@@ -121,33 +193,34 @@ function Main() {
         </p>
 
         <div style={{ display: "flex" }}>
-          <div className="tag">
-            <span className="tag-icon" />
-            Advertising banners
-          </div>
-          <div className="tag">
-            <span className="tag-icon" />
-            YouTube video thumbnails
-          </div>
-          <div className="tag">
-            <span className="tag-icon" />
-            YouTube channel design
-          </div>
-          <div className="tag">
-            <span className="tag-icon" />
-            Infographics
+          <div style={{ display: "flex", flexWrap: "wrap", marginTop: "16px" }}>
+            <div className="tag">
+              <span className="tag-icon" />
+              Advertising banners
+            </div>
+            <div className="tag">
+              <span className="tag-icon" />
+              YouTube video thumbnails
+            </div>
+            <div className="tag">
+              <span className="tag-icon" />
+              YouTube channel design
+            </div>
+            <div className="tag">
+              <span className="tag-icon" />
+              Infographics
+            </div>
+            <div className="tag">
+              <span className="tag-icon" />
+              Instagram design
+            </div>
+            <div className="tag">
+              <span className="tag-icon" />
+              Facebook design
+            </div>
           </div>
         </div>
-        <div style={{ display: "flex", marginTop: "16px" }}>
-          <div className="tag">
-            <span className="tag-icon" />
-            Instagram design
-          </div>
-          <div className="tag">
-            <span className="tag-icon" />
-            Facebook design
-          </div>
-        </div>
+
         <p style={{ fontSize: "27px" }}>
           Open to discussing design creation and other areas. <br />
           I'm ready to discuss the details in{" "}
@@ -202,6 +275,70 @@ function Main() {
           <RenderComponent />
         </div>
       </div>
+
+      <div className="review-block">
+        <h1>REVIEWS</h1>
+        <p className="description">
+          Client reviews, written from their own
+          <span className="selecting"> personal accounts </span>
+          on Telegram. Everything is transparent! <br /> Any review can be
+          <span className="selecting"> opened </span> in Telegram, and you can
+          <span className="selecting"> ask </span> about the experience of
+          working with me <br />
+          from the review's author directly.
+        </p>
+
+        <div className="review-carousel">
+          <div className="review-container" ref={containerReviewRef}>
+            {reviews.slice(-visibleReviews)}
+            {reviews}
+            {reviews.slice(0, visibleReviews)}
+          </div>
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div className="next-button" style={{ transform: "rotate(180deg)" }}>
+            <p className="array-next-icon" onClick={btnPrevReview} />
+          </div>
+          <div className="next-button">
+            <p className="array-next-icon" onClick={btnNextReview} />
+          </div>
+        </div>
+      </div>
+
+      <div className="guarantees-block">
+        <h1 style={{ fontSize: "52px", paddingBottom: "20px" }}>GUARANTEES</h1>
+
+        <ol className="guarantees-points">
+          <li className="point">
+            I accept payment through the
+            <span style={{ color: "#4824ff" }}> Name* </span>
+            payment system, which ensures <br />
+            the security of money transfers.
+          </li>
+          <li className="point">
+            You can verify my responsibility and professionalism by
+            <span style={{ color: "#4824ff" }}> messaging the clients </span>
+            <br /> who left reviews
+            <span style={{ color: "#4824ff" }}> directly </span>
+            at any time (client reviews are above).
+          </li>
+          <li className="point">
+            All
+            <span style={{ color: "#4824ff" }}> copyright </span>
+            for the work passes to the client after the order is completed.
+          </li>
+          <li className="point">
+            In my work I use materials strictly
+            <span style={{ color: "#4824ff" }}>
+              {" "}
+              licensed for personal <br /> and commercial use
+            </span>
+            .
+          </li>
+        </ol>
+      </div>
+      <div className="footer"> Workford </div>
     </div>
   );
 }
