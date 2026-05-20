89bc823c1fee3dc4d652ba3193bf5a61a462d703
AlexGochu <gochualex@gmail.com>
2026-05-20 18:39:02 +0100

    Creates first step for Main.tsx correction

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Main from "./Main";
+import Main from "./_main";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
